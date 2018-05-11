47a52981c2ea2c26439042c60c0255eacd5f19f9
shraddhsskavathekar <kavathekar2017@gmail.com>
2018-05-12 00:25:10 +0530

    Add files via upload

---
+++ TestException1.java
@@ -0,0 +1,33 @@
+class InvalidAgeException extends Exception
+{
+	InvalidAgeException(String s)
+	{
+		super(s);
+	}
+}
+
+class TestException1
+{
+	static void validate(int age) throws InvalidAgeException
+	{
+		if(age<18)
+		{
+			throw new InvalidAgeException("not valid");
+		}
+		else
+		{
+			System.out.println("Welcomt to vote");
+		}
+	}
+	public static void main(String args[])
+	{
+		try
+		{
+			validate(12);
+		}
+		catch(Exception m)
+		{
+			System.out.println("Exception Occured:"+m);
+		}
+	}
+}
